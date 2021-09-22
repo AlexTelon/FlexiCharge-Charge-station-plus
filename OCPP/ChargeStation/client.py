a4699733fa046ae024c9cb0e78b53aba629166c1
azaqis <55734899+azaqis@users.noreply.github.com>
2021-09-22 11:17:45 +0200

    after_start_transaction added
    Added the basics of after_start_transaction. More will be added to it when the screen and ocpp will be implemented togheter

--- OCPP/ChargeStation/client.py
+++ OCPP/ChargeStation/client.py
@@ -4,12 +4,18 @@ from typing import Dict
 import websockets
 import datetime
 from threading import Thread
-from ocpp.v16 import call, ChargePoint as cp
-from ocpp.v16.enums import AuthorizationStatus, RegistrationStatus
+from ocpp.routing import on, after
+from ocpp.v16 import ChargePoint as cp
+from ocpp.v16.enums import Action, RegistrationStatus, AuthorizationStatus, DataTransferStatus, ChargePointStatus
+from ocpp.v16 import call_result, call
 #import _thread
 #import nest_asyncio
 
 class ChargePoint(cp):
+    def __init__(self, id, connection, response_timeout=30):
+        super().__init__(id, connection, response_timeout=response_timeout)
+        self.status = ChargePointStatus.available
+
     async def send_boot_notification(self):
         request = call.BootNotificationPayload(
             charge_point_model="Optimus",
@@ -33,12 +39,11 @@ class ChargePoint(cp):
             print("Authorized.")
 
     #Sends a heartbeat to Centralstation, print the result from Centralstation
-    #TODO According to OCPP-documentation, Chargepoint internal clock should be synced with Centralstation's clock (which the heartbeat response contains).
-    #Will add this when correct time is needed for our Chargepoint!
+    #####TODO According to OCPP-documentation, Chargepoint internal clock should be synced with Centralstation's clock (which the heartbeat response contains).
+    #####Will add this when correct time is needed for our Chargepoint!
     async def on_heartbeat(self):
         request = call.HeartbeatPayload()
         response = await self.call(request)
-
         print("Heartbeat: " + str(response))
 
     #Sends a heartbeat to Centralstation periodically
@@ -47,22 +52,32 @@ class ChargePoint(cp):
             await self.on_heartbeat()
             await asyncio.sleep(interval)
 
+    #Sets status to charging after transaction has been started. Here we would also like to update the screen when such functionality is implemented!
+    @after(Action.StartTransaction)
+    def after_start_transaction(self):
+        self.status = ChargePointStatus.charging
+        print("Transaction status is set to: " + self.status)
+
 #This is a coroutine running in paralell with other coroutines
 async def user_input_task(cp):
     a = 0
     while 1:
         #a = input(">> ")
         if a == 0:
-            print("Testing 1")
+            print("Testing " + str(a))
             await asyncio.gather(cp.send_boot_notification())
         elif a == 1:
-            print("Testing 2")
+            print("Testing " + str(a))
             await asyncio.gather(cp.send_authorization())
         elif a == 2:
-            print("Testing 3")
+            print("Testing " + str(a))
             await asyncio.gather(cp.on_heartbeat())
+        elif a == 4:
+            print("Testing " + str(a))
+            cp.after_start_transaction()
+        
         await asyncio.sleep(5)
-        a = (a + 1) % 3
+        a = (a + 1) % 4
 
 async def main():
     async with websockets.connect(
