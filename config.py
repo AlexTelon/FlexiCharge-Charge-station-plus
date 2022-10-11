c204587f87da82502b79b799c5079f2243d39c12
bagen1 <elinbage22@gmail.com>
2022-10-11 09:09:25 +0200

    We can now get the remote start request and the charging state will show on the screen
    It will crash in the charging state due to something in the charging state

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 class Configurations():
 
     def __init__(self):
-        self._useMockServer = False
+        self._useMockServer = True
         self._mockServerIP = 'ws://127.0.0.1:60003'
         self._iPAddress = "ws://18.202.253.30:1337/charger/"
         self._chargerSerialNumber = "testnumber13"
